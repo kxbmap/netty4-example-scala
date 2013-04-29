797d45f6bbdf4ad62add3d504a3abf053cf1b5eb
kxbmap <kxbmap@gmail.com>
2013-04-29 14:41:19 +0900

    Use MODULE_SUFFIX_STRING instead of $

--- src/main/scala/com/github/kxbmap/netty/example/Usage.scala
+++ src/main/scala/com/github/kxbmap/netty/example/Usage.scala
@@ -1,5 +1,6 @@
 package com.github.kxbmap.netty.example
 
+import scala.reflect.NameTransformer
 import scala.util.control.NonFatal
 
 trait Usage {
@@ -7,7 +8,7 @@ trait Usage {
 
   private def showUsage(usage: String): Nothing = {
     Console.err.println(
-      s"Usage: ${getClass.getSimpleName stripSuffix "$"} $usage")
+      s"Usage: ${getClass.getSimpleName stripSuffix NameTransformer.MODULE_SUFFIX_STRING} $usage")
     sys.exit()
   }
 
