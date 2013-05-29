11c8e10b1713cfe610c4eab077c62a7a9afeb996
kxbmap <kxbmap@gmail.com>
2013-05-30 02:28:01 +0900

    Few refactoring Usage

--- src/main/scala/com/github/kxbmap/netty/example/Usage.scala
+++ src/main/scala/com/github/kxbmap/netty/example/Usage.scala
@@ -3,12 +3,12 @@ package com.github.kxbmap.netty.example
 import scala.reflect.NameTransformer
 import scala.util.control.NonFatal
 
-trait Usage {
-  this: App =>
+trait Usage { this: App =>
+
+  def programName = getClass.getSimpleName stripSuffix NameTransformer.MODULE_SUFFIX_STRING
 
   private def showUsage(usage: String): Nothing = {
-    Console.err.println(
-      s"Usage: ${getClass.getSimpleName stripSuffix NameTransformer.MODULE_SUFFIX_STRING} $usage")
+    Console.err.println(s"Usage: $programName $usage")
     sys.exit()
   }
 
@@ -16,4 +16,5 @@ trait Usage {
     try p(args.toList) catch {
       case NonFatal(_) => showUsage(usage)
     }
+
 }
