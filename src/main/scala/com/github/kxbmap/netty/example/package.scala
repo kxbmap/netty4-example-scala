dce503a91573d1ca2f3c9901fdb2d4366f500920
kxbmap <kxbmap@gmail.com>
2013-07-21 23:51:16 +0900

    Add utilities for GenericFuture

--- src/main/scala/com/github/kxbmap/netty/example/package.scala
+++ src/main/scala/com/github/kxbmap/netty/example/package.scala
@@ -1,38 +1,64 @@
 package com.github.kxbmap.netty
 
 import io.netty.channel.{ChannelFutureListener, ChannelFuture, ChannelInitializer, Channel}
+import io.netty.util.concurrent.{GenericFutureListener, Future}
 import scala.util.{Failure, Success, Try}
 
 package object example {
+
+  import scala.language.implicitConversions
+
 
   implicit final class Tapper[T](val obj: T) extends AnyVal {
     def <|[U](f: T => U): T = { f(obj); obj }
   }
 
-  import scala.language.implicitConversions
 
-  implicit def ToChannelInitializer[C <: Channel, U](f: C => U): ChannelInitializer[C] =
+  implicit def toChannelInitializer[C <: Channel, U](f: C => U): ChannelInitializer[C] =
     new ChannelInitializer[C] {
       def initChannel(ch: C): Unit = f(ch)
     }
 
-  implicit def ToChannelFutureListener[U](f: ChannelFuture => U): ChannelFutureListener =
-    new ChannelFutureListener {
-      def operationComplete(future: ChannelFuture): Unit = { f(future) }
+
+  implicit def toGenericFutureListener[T, U](f: Future[T] => U): GenericFutureListener[Future[T]] =
+    new GenericFutureListener[Future[T]] {
+      def operationComplete(future: Future[T]): Unit = f(future)
     }
 
+  implicit final class GenericFutureOps[T](val gf: Future[T]) extends AnyVal {
 
-  implicit final class ChannelFutureOps(val cf: ChannelFuture) extends AnyVal {
-
-    def onSuccess[U](pf: PartialFunction[Channel, U]): Unit =
+    def onSuccess[U](pf: PartialFunction[T, U]): Unit =
       onComplete {
-        case Success(ch) if pf.isDefinedAt(ch) => pf(ch)
+        case Success(v) if pf.isDefinedAt(v) => pf(v)
         case _ =>
       }
 
     def onFailure[U](pf: PartialFunction[Throwable, U]): Unit =
       onComplete {
         case Failure(cause) if pf.isDefinedAt(cause) => pf(cause)
+        case _ =>
+      }
+
+    def onComplete[U](f: Try[T] => U): Unit =
+      gf.addListener { future: Future[T] => f(
+        if (future.isSuccess)
+          Success(future.getNow)
+        else
+          Failure(future.cause())
+      )}
+  }
+
+
+  implicit def toChannelFutureListener[U](f: ChannelFuture => U): ChannelFutureListener =
+    new ChannelFutureListener {
+      def operationComplete(future: ChannelFuture): Unit = f(future)
+    }
+
+  implicit final class ChannelFutureOps(val cf: ChannelFuture) extends AnyVal {
+
+    def onSuccess[U](pf: PartialFunction[Channel, U]): Unit =
+      onComplete {
+        case Success(ch) if pf.isDefinedAt(ch) => pf(ch)
         case _ =>
       }
 
